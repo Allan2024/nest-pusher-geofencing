ba8b0e1834bb7b54c0d5c4a796d9d40ce42c7994
Allan Joseph <87084023+Allan2024@users.noreply.github.com>
2022-10-30 21:56:21 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,22 @@ import { Get, Controller, Res } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
+  home(@Res() res) {
+    res.render('home');
+  }
+
+  @Get('/index')
   root(@Res() res) {
     res.render('index');
+  }
+
+  @Get('/about')
+  about(@Res() res) {
+    res.render('about');
+  }
+
+  @Get('/contact')
+  contact(@Res() res) {
+    res.render('contact');
   }
 }
